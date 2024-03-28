34f2a934721d9c21153640d4e166879dee96a6c9
Redakas <andregabrielmelo@gmail.com>
2024-03-28 19:32:08 -0300

    Inicio

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+// Struct lista 
+// Struct produto 
+// Struct carrinho (lista + vendedor + valor total)?
+
+// pesquisar lista
+// verificar valores de itens na lista 
+// reajustar preço de item na lista
+// remover itens da lista (do estoque)
+// controlar quantidade disponiveis e discontos de cada item na lista 
+// salvar lista em um arquivo
+// pegar lista de um arquivo
+
+int main() {
+    // Nova lista com os produtos a venda 
+
+    // Nova lista do cliente (carrinho de compras)
+
+    // Nova lista com os dados sobre vendas (valor, desconto, forma de pagamento, 
+    // total de vendas, total de vendas por forma de pagameto, total de vendar por vendedor, etc)
+
+
+    return 0;
+}
