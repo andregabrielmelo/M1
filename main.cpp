9c7c972b4071dc207c651a403bda62452b1bb2cc
Redakas <andregabrielmelo@gmail.com>
2024-03-28 20:11:02 -0300

    Adicionando structs e um pouco mais

--- main.cpp
+++ main.cpp
@@ -1,11 +1,37 @@
+#include <iostream>
 #include <stdlib.h>
 #include <stdio.h>
 
-// Struct lista 
+using namespace std;
+
 // Struct produto 
+struct Produto {
+    string nome;
+    float preco;
+    int quantidade_disponivel;
+};
+
+// Struct node (prduto + próximo produto)
+struct No {
+    Produto produto;
+    No *elo = nullptr;
+};
+
+// Struct lista encadeada
+struct LUE {
+    No *comeco = nullptr;
+    No *fim = nullptr;
+};
+
 // Struct carrinho (lista + vendedor + valor total)?
+struct Carrinho {
+    string vendedor;
+    int valor_total;
+    LUE *lista = nullptr;
+};
 
 // pesquisar lista
+bool pesquisar_lista(LUE *lista, Produto produto);
 // verificar valores de itens na lista 
 // reajustar preço de item na lista
 // remover itens da lista (do estoque)
