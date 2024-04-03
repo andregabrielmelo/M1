07aa6d195945a587cd06a15914378d4e25f820f6
Redakas <andregabrielmelo@gmail.com>
2024-04-03 19:36:54 -0300

    teste

--- main.cpp
+++ main.cpp
@@ -1,20 +1,33 @@
 #include <iostream>
 #include <stdlib.h>
 #include <stdio.h>
-
-// Hello world
+#include <locale.h>
 
 using namespace std;
 
+
+//teste// 
 // Struct produto 
 struct Produto {
     string nome;
     float preco;
     float desconto;
     int quantidade_disponivel;
+
+    // operator = overloading com o tipo Produto
+    Produto& operator=(const Produto& produto) {
+        if (this != &produto) { // Verifica se n√£o √© a mesma inst√¢ncia
+            // Atribui os membros individualmente
+            this->nome = produto.nome;
+            this->preco = produto.preco;
+            this->desconto = produto.desconto;
+            this->quantidade_disponivel = produto.quantidade_disponivel;
+        }
+        return *this;
+    }
 };
 
-// Struct node (prduto + prÛximo produto)
+// Struct node (prduto + pr√≥ximo produto)
 struct No {
     Produto produto;
     No *elo = nullptr;
@@ -43,23 +56,12 @@ bool operator !=(const Produto &produto1, const Produto &produto2) {
     return !(produto1.nome == produto2.nome && produto1.preco == produto2.preco && produto1.quantidade_disponivel == produto2.quantidade_disponivel);
 }
 
-// operator = overloading com o tipo Produto
-Produto& Produto::operator=(const Produto& produto) {
-    if (this != &produto) { // Verifica se n„o È a mesma inst‚ncia
-        // Atribui os membros individualmente
-        this->nome = produto.nome;
-        this->preco = produto.preco;
-        this->quantidade_disponivel = produto.quantidade_disponivel;
-    }
-    return *this;
-}
-
 int verificar_valor(LUE *lista, Produto produto); // verificar valores de itens na lista 
 bool imprimir_lista(LUE *lista); // mostrar lista
 bool inserir_final_lista(LUE *lista, Produto produto);// Inserir item na lista 
 bool remover_lista(LUE *lista, Produto produto); // remover itens da lista (do estoque)
 bool pesquisar_lista(LUE *lista, Produto produto); // pesquisar lista
-bool reajustar_preco(LUE *lista, int posicao, float preco); // reajustar preÁo de item na lista
+bool reajustar_preco(LUE *lista, int posicao, float preco); // reajustar pre√ßo de item na lista
 bool reajustar_quantidade(LUE *lista, int posicao, int quantidade); // controlar quantidade disponiveis 
 bool reajustar_desconto(LUE *lista, int posicao, float desconto); // controlar descontos 
 // salvar lista em um arquivo
@@ -68,10 +70,13 @@ bool reajustar_desconto(LUE *lista, int posicao, float desconto); // controlar d
 void decoracao(int numero);
 
 int main() {
+
+    setlocale(LC_ALL, "Portuguese_Brazil");
+
     // Nova lista com os produtos a venda 
     LUE *produtos_venda = new LUE;
     if (produtos_venda == nullptr) {
-        cout << "AlocaÁ„o de memÛria falhou";
+        cout << "Aloca√ß√£o de mem√≥ria falhou";
         return 1;
     }
 
@@ -83,12 +88,14 @@ int main() {
     blusa.quantidade_disponivel = 1;
 
     inserir_final_lista(produtos_venda, blusa);
+
+    cout << "\nProdutos a venda:\n";
     imprimir_lista(produtos_venda);
 
     // Nova lista do cliente (carrinho de compras)
     LUE *carrinho = new LUE;
     if (carrinho == nullptr) {
-        cout << "AlocaÁ„o de memÛria falhou";
+        cout << "Aloca√ß√£o de mem√≥ria falhou";
         return 1;
     }
 
@@ -98,27 +105,33 @@ int main() {
     while(true) {
 
         // Mostrar menu
-        cout << "\n" << decoracao << "\n";
-        cout << "\nEscolha uma das opÁıes:";
+        cout << "\n"; 
+        decoracao(100);
+        cout << "\n";
+
+        cout << "\nEscolha uma das op√ß√µes:";
         cout << "\n1.Cliente Comprando";
         cout << "\n2.Pesquisar";
         cout << "\n3.Verificar Valor";
-        cout << "\n4.Reajustar PreÁo";
+        cout << "\n4.Reajustar Pre√ßo";
         cout << "\n5.Remover Produto";
         cout << "\n6.Inserir Produto";
         cout << "\n7.Modificar Produto";
         cout << "\n8.Exit";
-        cout << "\n" << decoracao << "\n";
+        
+        cout << "\n"; 
+        decoracao(100);
+        cout << "\n";
 
         char choice;
-        while(true) { // O usu·rio deve escolher o que fazer na aplicaÁ„o 
-            cout << "\nRealizar a opÁ„o: ";
+        while(true) { // O usu√°rio deve escolher o que fazer na aplica√ß√£o 
+            cout << "\nRealizar a op√ß√£o: ";
             cin >> choice;
 
-            if (choice >= '1' && choice <= '8') { // Verifica a entrada do usu·rio
+            if (choice >= '1' && choice <= '8') { // Verifica a entrada do usu√°rio
                 break;
             } else {
-                cout << "\nEntrada inv·lida. Escolha uma n˙mero de 1 a 6.";
+                cout << "\nEntrada inv√°lida. Escolha uma n√∫mero de 1 a 6.";
             }
         } 
 
@@ -129,7 +142,7 @@ int main() {
                 break;
             case 3: // Verificar valor de um produto
                 break;
-            case 4: // Reajustar preÁo de um produto 
+            case 4: // Reajustar pre√ßo de um produto 
                 break;
             case 5: // Remover produto
                 break;
@@ -139,7 +152,8 @@ int main() {
                 break;
             case 8: // Sair do programa
                 cout << "\nSaindo do programa...";
-                // exclue os ponteiros? verificar se eles est„o excluidos?
+                break;
+                // exclue os ponteiros? verificar se eles est√£o excluidos?
                 break;
             default:
                 break;
@@ -151,94 +165,100 @@ int main() {
 }
 
 bool imprimir_lista(LUE *lista) {
-    No *temp = new No; // nÛ tempor·rio para atravessar a lista 
+    No *temp = new No; // n√≥ tempor√°rio para atravessar a lista 
 
-    // Equanto a lista n„o acabar
+    temp = lista->comeco; // Aponta para o come√ßo da lista
+
+    // Equanto a lista n√£o acabar
     while (temp != nullptr) {
-        cout << temp->produto.nome << "," << temp->produto.preco << "," << temp->produto.desconto << "," << temp->produto.quantidade_disponivel << "\n";
+        cout << "Nome: " << temp->produto.nome << "\n";
+        cout << "Pre√ßo: " << temp->produto.preco << "\n";
+        cout << "Desconto: " << temp->produto.desconto << "%\n";
+        cout << "Quantidade disponivel: " << temp->produto.quantidade_disponivel << "\n";
+
+        temp = temp->elo; // pr√≥ximo elemento
     }
 
     return true;
 }
 
-bool pesquisar_lista(LUE *lista, Produto produto) { // Mudar para pesquisar o produto pelo nome/preÁo. Talvez deve retornar o produto?
+bool pesquisar_lista(LUE *lista, Produto produto) { // Mudar para pesquisar o produto pelo nome/pre√ßo. Talvez deve retornar o produto?
     No *temp = lista->comeco; // no temporario para atravessar a lista
 
-    // Enquanto o elemento da lista n„o for nulo
+    // Enquanto o elemento da lista n√£o for nulo
     while (temp != nullptr) {
 
         if (temp->produto == produto) { // Se o produto foi encontrado na lista
             return true;
         }
 
-        temp = temp->elo; // PrÛximo elemento na lista 
+        temp = temp->elo; // Pr√≥ximo elemento na lista 
     }
 
     return false;
 }
 
 int verificar_valor(LUE *lista, Produto produto) {
-    No *temp = lista->comeco; // nÛ temporario para atravessar a lista
+    No *temp = lista->comeco; // n√≥ temporario para atravessar a lista
 
-    // Enquanto o elemento da lista n„o for nulo
+    // Enquanto o elemento da lista n√£o for nulo
     while (temp != nullptr) {
 
-        if (temp->produto == produto) { // Se o produto foi encontrado na lista, retorna o preÁo
+        if (temp->produto == produto) { // Se o produto foi encontrado na lista, retorna o pre√ßo
             return temp->produto.preco;
         }
 
-        temp = temp->elo; // PrÛximo produto na lista 
+        temp = temp->elo; // Pr√≥ximo produto na lista 
     }
 
     return 0;
 }
 
-bool inserir_final_lista(LUE *lista, Produto produto) { // Verificar novamente em uma revis„o final
+bool inserir_final_lista(LUE *lista, Produto produto) { // Verificar novamente em uma revis√£o final
 
-    // Novo nÛ, para o novo produto
+    // Novo n√≥, para o novo produto
     No *novo_no = new No;
     novo_no->produto = produto;
+    novo_no->elo = nullptr;
 
-    if (novo_no == nullptr) { // Verificar se a alocaÁ„o de memoria deu certo 
+    if (novo_no == nullptr) { // Verificar se a aloca√ß√£o de memoria deu certo 
         return false;
     }
 
     if (lista->comeco == nullptr) { // Caso a lista esteja vazia
         lista->comeco = novo_no;
-        lista->fim = novo_no;
+        lista->fim = novo_no; // muda o fim da lista
     } else {
-        lista->fim->elo = novo_no; // Adiciona um prÛximo nÛ para o ultimo da lista
+        lista->fim->elo = novo_no; // Adiciona um pr√≥ximo n√≥ para o ultimo da lista
     }
-
-    lista->fim = novo_no; // muda o fim da lista
 
     return true;
 }
 
 bool remover_lista(LUE *lista, Produto produto) {
-    No *temp = lista->comeco; // nÛ temporario para atravessar a lista
-    No *temp_anterior = nullptr; // guarda o nÛ anterior do temp
+    No *temp = lista->comeco; // n√≥ temporario para atravessar a lista
+    No *temp_anterior = nullptr; // guarda o n√≥ anterior do temp
 
-    // Enquanto o elemento da lista n„o for nulo
+    // Enquanto o elemento da lista n√£o for nulo
     while (temp != nullptr && temp->produto != produto) {
         temp_anterior = temp;
-        temp = temp->elo; // PrÛximo produto na lista 
+        temp = temp->elo; // Pr√≥ximo produto na lista 
     }
 
-    if (temp == lista->comeco && temp == lista->fim) { // a lista contÈm um unico elemento
+    if (temp == lista->comeco && temp == lista->fim) { // a lista cont√©m um unico elemento
         lista->comeco = nullptr;
         lista->fim = nullptr;
         delete temp;
         return true;
     }
 
-    if (temp == lista->comeco) { // se o elemento a ser excluido È o primeiro da lista
+    if (temp == lista->comeco) { // se o elemento a ser excluido √© o primeiro da lista
         lista->comeco = temp->elo;
         delete temp;
         return true;
     }
 
-    if (temp == lista->fim) { // se o elemento a ser excluido È o ˙ltimo da lista
+    if (temp == lista->fim) { // se o elemento a ser excluido √© o √∫ltimo da lista
         temp_anterior->elo =  nullptr;
         lista->fim = temp_anterior;
         delete temp;
@@ -251,20 +271,20 @@ bool remover_lista(LUE *lista, Produto produto) {
 }
 
 bool reajustar_preco(LUE *lista, int posicao, float preco) {
-    if (lista == nullptr || lista->comeco == nullptr) { // verfica se a lista È valida
+    if (lista == nullptr || lista->comeco == nullptr) { // verfica se a lista √© valida
         return false;
     }
 
-    No *temp = lista->comeco; // nÛ temporario para atravessar a lista
+    No *temp = lista->comeco; // n√≥ temporario para atravessar a lista
     int i = 0;
 
-    // Percorra a lista atÈ a posiÁ„o desejada
+    // Percorra a lista at√© a posi√ß√£o desejada
     while (i != posicao) {
         i++;
-        temp = temp->elo; // PrÛximo produto na lista 
+        temp = temp->elo; // Pr√≥ximo produto na lista 
     }
 
-    if (temp == nullptr) { // Verifica se a posiÁ„o È valida
+    if (temp == nullptr) { // Verifica se a posi√ß√£o √© valida
         return false;
     }
 
@@ -274,20 +294,20 @@ bool reajustar_preco(LUE *lista, int posicao, float preco) {
 }
 
 bool reajustar_quantidade(LUE *lista, int posicao, int quantidade) {
-    if (lista == nullptr || lista->comeco == nullptr) { // verfica se a lista È valida
+    if (lista == nullptr || lista->comeco == nullptr) { // verfica se a lista √© valida
         return false;
     }
 
-    No *temp = lista->comeco; // nÛ temporario para atravessar a lista
+    No *temp = lista->comeco; // n√≥ temporario para atravessar a lista
     int i = 0;
 
-    // Percorra a lista atÈ a posiÁ„o desejada
+    // Percorra a lista at√© a posi√ß√£o desejada
     while (i != posicao) {
         i++;
-        temp = temp->elo; // PrÛximo produto na lista 
+        temp = temp->elo; // Pr√≥ximo produto na lista 
     }
 
-    if (temp == nullptr) { // Verifica se a posiÁ„o È valida
+    if (temp == nullptr) { // Verifica se a posi√ß√£o √© valida
         return false;
     }
 
@@ -297,20 +317,20 @@ bool reajustar_quantidade(LUE *lista, int posicao, int quantidade) {
 }
 
 bool reajustar_desconto(LUE *lista, int posicao, float desconto) { 
-    if (lista == nullptr || lista->comeco == nullptr) { // verfica se a lista È valida
+    if (lista == nullptr || lista->comeco == nullptr) { // verfica se a lista √© valida
         return false;
     }
 
-    No *temp = lista->comeco; // nÛ temporario para atravessar a lista
+    No *temp = lista->comeco; // n√≥ temporario para atravessar a lista
     int i = 0;
 
-    // Percorra a lista atÈ a posiÁ„o desejada
+    // Percorra a lista at√© a posi√ß√£o desejada
     while (i != posicao) {
         i++;
-        temp = temp->elo; // PrÛximo produto na lista 
+        temp = temp->elo; // Pr√≥ximo produto na lista 
     }
 
-    if (temp == nullptr) { // Verifica se a posiÁ„o È valida
+    if (temp == nullptr) { // Verifica se a posi√ß√£o √© valida
         return false;
     }
 
